5922b6319248637df275d27ce0e5d984cd10aa98
David Winterbottom <david.winterbottom@gmail.com>
2013-04-12 09:55:29 +0100

    Relax purl requirement

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ PROJECT_ROOT = os.path.dirname(__file__)
 
 setup(
     name='django-oscar-testsupport',
-    version='0.3',
+    version='0.3.1',
     url='https://github.com/tangentlabs/django-oscar-testsupport',
     author="David Winterbottom",
     author_email="david.winterbottom@tangentlabs.co.uk",
@@ -19,5 +19,5 @@ setup(
         'django-oscar>=0.4,<0.6',
         'WebTest>=1.3,<2.1',
         'django-webtest>=1.5.7,<1.6',
-        'purl>=0.4,<0.5',
+        'purl>=0.4',
     ])
